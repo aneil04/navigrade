48b674123ddaeb8c1c34db87015088306bc29a0e
aneil04 <aneil2642@gmail.com>
2022-11-13 06:34:00 -0500

    styles

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ import { usePenaltyContext } from "../PenaltyContext";
 import Maps from "./Maps";
 import FaceTrack from "./FaceTrack";
 
+import "../styles.css"
+
 export default function App() {
   return (
     <div>
@@ -25,7 +27,19 @@ function Score() {
       <div>f{focus}</div>
       <div>s{speedPenalty}</div>
       <div>a{awareness}</div>
-      <div>score: {100 - focus - speedPenalty - awareness}</div>
+      <div id="score">{100 - focus - speedPenalty - awareness}</div>
+      <StackElement text={"SPEED"} value={speedPenalty}></StackElement>
+      <StackElement text={"SPEED"} value={speedPenalty}></StackElement>
+      <StackElement text={"SPEED"} value={speedPenalty}></StackElement>
     </div>
   );
+}
+
+function StackElement({text, value}) {
+    return (
+        <div className="stack-element">
+            <div className="stack-txt1">{text}</div>
+            <div className="stack-txt2">{value}</div>
+        </div>
+    )
 }
