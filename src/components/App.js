ae98e5c56185e73f50b0c062f945fdebe3fdcbfd
aneil04 <aneil2642@gmail.com>
2022-11-13 07:03:48 -0500

    css?

--- src/components/App.js
+++ src/components/App.js
@@ -8,34 +8,35 @@ import FaceTrack from "./FaceTrack";
 import "../styles.css"
 
 export default function App() {
-  return (
-    <div>
-      <PenaltyProvider>
-        <Score></Score>
-        <FaceTrack></FaceTrack>
-        <Maps></Maps>
-      </PenaltyProvider>
-    </div>
-  );
+    return (
+        <div style={{ width: '60%', flexGrow: '1', margin:'auto'}}>
+            <div style={{ width: '60%', flexGrow: '1', margin:'auto', backgroundColor:'blue'}}>
+                <PenaltyProvider>
+                    <Score></Score>
+                    <FaceTrack></FaceTrack>
+                    <Maps></Maps>
+                </PenaltyProvider>
+            </div>
+        </div>
+    );
 }
 
 function Score() {
-  const { focus, speedPenalty, awareness } = usePenaltyContext();
+    const { focus, speedPenalty, awareness } = usePenaltyContext();
 
-  return (
-    <div>
-      <div>f{focus}</div>
-      <div>s{speedPenalty}</div>
-      <div>a{awareness}</div>
-      <div id="score">{100 - focus - speedPenalty - awareness}</div>
-      <StackElement text={"SPEED"} value={speedPenalty}></StackElement>
-      <StackElement text={"SPEED"} value={speedPenalty}></StackElement>
-      <StackElement text={"SPEED"} value={speedPenalty}></StackElement>
-    </div>
-  );
+    return (
+        <div>
+            <div id="score">{100 - focus - speedPenalty - awareness}</div>
+            <div id="stack">
+                <StackElement text={"SPEED"} value={speedPenalty}></StackElement>
+                <StackElement text={"FOCUS"} value={focus}></StackElement>
+                <StackElement text={"AWARENESS"} value={awareness}></StackElement>
+            </div>
+        </div>
+    );
 }
 
-function StackElement({text, value}) {
+function StackElement({ text, value }) {
     return (
         <div className="stack-element">
             <div className="stack-txt1">{text}</div>
