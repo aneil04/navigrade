94f3b976c8c5fda5bad7be22151b1fc514c82c98
Neal <nealshah04@gmail.com>
2022-11-13 06:29:58 -0500

    fully functional backend

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -1,51 +1,38 @@
 import { useEffect, useState } from "react";
 import { usePenaltyContext } from "../PenaltyContext";
 
+let looked;
 const tInterval = 200;
 
 export default function Maps() {
-  const { awareness, setAwareness, deductSpeed, lookedLR, setLookedLR } =
-    usePenaltyContext();
+  const {
+    awareness,
+    setAwareness,
+    deductSpeed,
+    lookedLR,
+    setLookedLR,
+    speedPenalty,
+    setSpeedPenalty,
+    deductAwareness,
+  } = usePenaltyContext();
 
+  let stopped = false;
   const [curr, setCurr] = useState({
     speed: 0,
     longitude: 0,
     latitude: 0,
     accuracy: 0,
   });
-
+  let stop = false;
   useEffect(() => {
     const interval = setInterval(() => {
       navigator.geolocation.getCurrentPosition(success, error, options);
-
-      if (curr.speed * 2.237 > 25) {
-        deductSpeed();
-      }
-      let stopped = true;
-      if (curr.speed === 0 && !stopped) {
-        //stopped after driving
-        stopped = true;
-        console.log("stopped");
-      } else if (stopped && curr.speed > 3) {
-        //started to drive from stop
-        if (!lookedLR) {
-          //didn't look
-          console.log("forgot to look!");
-          // setAwareness(5);
-        }
-        console.log("looked!");
-        stopped = false;
-        setLookedLR(false);
-      } else if (lookedLR && curr.speed > 3) {
-        //driving and looked, which is useless
-        setLookedLR(false);
-      }
+      looked = lookedLR;
     }, tInterval);
-
     return () => {
       clearInterval(interval);
     };
-  }, []);
+  }, [lookedLR]);
 
   const options = {
     enableHighAccuracy: true,
@@ -54,6 +41,41 @@ export default function Maps() {
   };
 
   function success(pos) {
+    // if (pos.coords.speed === 0 && !stopped) {
+    //   //stopped after driving
+    //   stopped = true;
+    //   console.log("stopped");
+    // } else if (stopped && pos.coords.speed > 0) {
+    //   //started to drive from stop
+    //   if (!looked) {
+    //     //didn't look
+    //     console.log("forgot to look!");
+    //     deductAwareness();
+    //     // setAwareness(5);
+    //   }
+    //   console.log("looked!");
+    //   stopped = false;
+    //   looked = false;
+    // } else if (looked && !stopped) {
+    //   //driving and looked, which is useless
+    //   looked = false;
+    // }
+
+    if (pos.coords.speed === 0 && !stop) {
+      stop = true;
+      looked = false;
+    }
+    if (stop && pos.coords.speed > 1) {
+      stop = false;
+      if (!looked) {
+        deductAwareness();
+      }
+      setLookedLR(false);
+    }
+
+    if (pos.coords.speed * 2.237 > 25) {
+      deductSpeed();
+    }
     setCurr(pos.coords);
   }
 
@@ -82,6 +104,8 @@ export default function Maps() {
         src={directionsToRestStop}
         title="Directions to Rest Stop"
       ></iframe>
+      <div>Stopped: {curr.speed === 0 ? "yes" : "no"}</div>
+      <div>Looked left and right: {looked ? "yes" : "no"}</div>
     </div>
   );
 }
